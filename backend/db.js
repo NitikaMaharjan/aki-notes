f3b9a636eb795620e7f06d474af61bffad2dbdae
Keppiekip <maharjanitika@gmail.com>
2025-07-16 19:31:37 +0545

    created routes and schema for storing notes and user

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/";
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
