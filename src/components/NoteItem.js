7699b667f4e132848f22a43e2d8dacb0ecca274f
Keppiekip <maharjanitika@gmail.com>
2025-08-12 13:49:47 +0545

    added edit and delete button in note view modal

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -7,12 +7,15 @@ export default function NoteItem(props) {
 
   return (
     <>
-      <div className="card" data-bs-theme={`${theme=="light"?"light":"dark"}`} style={{cursor: "pointer"}} onClick={props.OpenModal}>
+      <div className="card" data-bs-theme={`${theme=="light"?"light":"dark"}`} style={{cursor: "pointer", width: "100%", height: "192px"}} onClick={props.OpenModal}>
           <div className="card-body">
               <h5 className="card-title" style={{color: `${theme=="light"?"black":"white"}`}}>{props.note.title.length<=18?props.note.title:props.note.title.slice(0,18)+"..."}</h5>
-              <h6 className="card-subtitle my-2 text-body-secondary">{props.note.tag}</h6>
-              <p className="card-subtitle my-2 text-body-secondary">{props.note.description.length<=100?props.note.description:props.note.description.slice(0,100)+"..."}</p>
-              <p className="card-subtitle text-body-secondary">{new Intl.DateTimeFormat('en-US', { year: 'numeric', day: 'numeric', month: 'short' }).format(date)}</p>
+              <div className="mb-2" style={{display: "flex"}}>
+                <p className="card-subtitle text-body-secondary" style={{margin: "0px", padding: "0px", fontSize: "14px", fontWeight: "500"}}>{props.note.tag}</p>
+                <p className="card-subtitle text-body-secondary mx-1" style={{margin: "0px", padding: "0px", fontSize: "14px"}}>|</p>
+                <p className="card-subtitle text-body-secondary" style={{margin: "0px", padding: "0px", fontSize: "14px"}}>{new Intl.DateTimeFormat('en-US', { year: 'numeric', day: 'numeric', month: 'short' }).format(date)}</p>
+              </div>
+              <p className="card-text">{props.note.description.length<=100?props.note.description:props.note.description.slice(0,100)+"..."}</p>
           </div>
       </div>
   </>
--- src/components/ShowNote.js
+++ src/components/ShowNote.js
@@ -5,9 +5,13 @@ import NoteItem from './NoteItem';
 export default function Note() {
     const {notes, setNotes} = useContext(NoteContext);
     const [selectedNote, setSelectedNote] = useState(null);
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState(null);
     
     const OpenModal = (note) => {
         setSelectedNote(note);
+        setDate(new Date(note.date.slice(0,10)));
+        setTime(new Date(note.date));
         const modal = new window.bootstrap.Modal(document.getElementById("exampleModal"));
         modal.show();
     };
@@ -20,21 +24,30 @@ export default function Note() {
                 })}
             </div>
 
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">{selectedNote?.title}</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade bd-example-modal-lg" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header d-flex justify-content-between" style={{border: "none"}}>
+                            <h5 className="modal-title" id="exampleModalLabel">{selectedNote?.title}</h5>
+                            <div>
+                                <button className="modal-btn"><img src="/icons/edit.png" alt="edit" title="edit"/></button>
+                                <button className="modal-btn"><img src="/icons/delete.png" alt="edit" title="delete"/></button>
+                                <button className="modal-btn" data-bs-dismiss="modal" aria-label="Close"><img src="/icons/close.png" alt="edit" title="close"/></button>
+                            </div>
+                        </div>
+                        <div style={{display: "flex", alignItems: "center", margin: "0px", padding: "0px 16px"}}>
+                            <h6 style={{margin: "0px", padding: "0px", color: "#212529bf"}}>{selectedNote?.tag}</h6>
+                            <h6 className="mx-2" style={{margin: "0px", padding: "0px", color: "#212529bf"}}>|</h6>
+                            <p style={{margin: "0px", padding: "0px", color: "#212529bf"}}>{new Intl.DateTimeFormat('en-US', { year: 'numeric', day: 'numeric', month: 'short' }).format(date)}</p>                           
+                            <h6 className="mx-2" style={{margin: "0px", padding: "0px", color: "#212529bf"}}>|</h6>
+                            <p style={{margin: "0px", padding: "0px", color: "#212529bf"}}>{time?.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>
+                            <h6 className="mx-2" style={{margin: "0px", padding: "0px", color: "#212529bf"}}>|</h6>
+                            <p style={{margin: "0px", padding: "0px", color: "#212529bf"}}>{selectedNote?.description.trim().length} characters</p>
+                        </div>
+                        <div className="modal-body">
+                            {selectedNote?.description}
+                        </div>
                     </div>
-                    <div className="modal-body">
-                    {selectedNote?.description}
-                    </div>
-                    <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
                 </div>
             </div>
         </>
